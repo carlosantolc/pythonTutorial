13068a9a95eae72e49cb6006794c9e42ec3e7164
carlosantolc <carlosantolc@github.com>
2020-06-11 22:07:54 +0200

    Variables. Videos hasta 09

---
+++ variables.py
@@ -0,0 +1,28 @@
+# Vídeo 09 - Variables
+# Una variable es un espacio en memoria capaz de almacenar información
+
+# El operador de asignación es el símbolo = y nos permite asignar un valor a una variable
+
+numero = 10
+print(numero)
+
+# Nombre de variables reservadas
+help("keywords")
+
+# En interprete de python, para otros nombres que se pueden usar pero que no se recomiendan
+# dir(__builtins__)
+
+numero1, numero2, numero3 = 1, 2, 3
+print(numero1)
+print(numero2)
+print(numero3)
+
+# Saber identificador en memoria de las variables
+# id(numero1)
+# id(numero2)
+
+# Para borrar la variable de memoria
+# del(numero1)
+
+# Saber variables en memoria
+# dir()
