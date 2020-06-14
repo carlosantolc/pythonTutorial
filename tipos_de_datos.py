0d5fcc33e4df11c4a899d86c3d98fb60329ca050
carlosantolc <carlosantolc@github.com>
2020-06-14 19:15:21 +0200

    Tipos de datos. Vídeos 10 a 12

---
+++ tipos_de_datos.py
@@ -0,0 +1,35 @@
+# En python tenemos 2 tipos de datos "NUMÉRICOS Y CADENAS" aunque también
+# podríamos decir que existen los booleanos
+
+# NUMÉRICOS
+# Enteros (int)
+entero = 10
+
+# Flotantes (float)
+floante = 10.05
+
+# Complejos (complex)
+complejo = (10.05+0j)
+
+# BOOLEANOS (bool)
+# Verdadero (True) y Falso (False)
+verdadero = True
+falso = False
+
+bool(0)     # Falso
+bool(2)     # True
+bool(-2)    # True
+bool(0.0)   # False
+bool(0.1)   # True
+bool("")    # False
+bool("Hola")# True
+
+# CADENA DE TEXTO (str)
+cadena = "Hola mundo"
+cadena2 = 'Hola mundo'
+
+print(cadena)
+print(cadena2)
+
+cadena3 = "Esto es una cadena\nEstoy en otra \"línea\""
+print(cadena3)
